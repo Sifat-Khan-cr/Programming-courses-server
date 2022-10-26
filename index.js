8660c4d6e6d40e28450eb02a57e156cd48a7aa20
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-10-26 20:02:43 +0600

    some data created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const data = require('./data/course.json')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,11 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("running");
 })
+app.get('/courses', (req, res) => {
+    res.send(data);
+})
+
+
 app.listen(port, () => {
     console.log('running', port);
 })
